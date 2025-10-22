df4365fec2a343100b8b3d287ad95c5c66072765
Hgiddal <helena.barddal@escola.pr.gov.br>
2025-10-22 07:55:04 -0300

    Aula 4 e 5

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ function mostraPalavrasChave() {
 
   const campoResultado = document.querySelector("#resultado-palavrachave");
 
-  const palavras = texto.split(" ");
+  const mostraPalavrasChave = processoTexto("texto");
 
-  campoResultado.textContent = palavras.join(", ");
+  campoResultado.textContent = palavraChave.join(", ");
 } 
+
+function processoTexto(texto){
+  let palavras = texto.split(/\P{L}+/u);
+
+  let frequencias = {};
+  for (let i of palavras){
+    frequencias[i] = 0;
+    for (let j of palavras){
+      if (i == j){
+        frequencias[i]++;
+      }
+    }
+  }
+
+  console.log(frequencias);
+
+  return palavras;
+}
