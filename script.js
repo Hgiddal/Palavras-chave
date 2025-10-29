58bf1a132075edd820fd3b54bf3d65d4852a400f
Hgiddal <helena.barddal@escola.pr.gov.br>
2025-10-29 07:10:14 -0300

    corrigindo

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
-  const texto = document.querySelector("#entrada-de-texto").value;
+  const texto = document.querySelector('#entrada-de-texto').value;
 
-  const campoResultado = document.querySelector("#resultado-palavrachave");
+  const campoResultado = document.querySelector('#resultado-palavrachave');
 
-  const mostraPalavrasChave = processoTexto("texto");
+  const mostraPalavrasChave = processoTexto(texto);
 
   campoResultado.textContent = palavraChave.join(", ");
 } 
